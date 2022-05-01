b5384069d8540b16cbbae07a54a18131750fd4f6
Danyal Akhtar <danyalakhtar1998@gmail.com>
2022-05-01 20:18:04 +0530

    First Commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import {feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+  const [pins, setPins] = useState(null);
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+
+
+  if (loading) {
+    return (
+      <Spinner message="We are adding new ideas to your feed!" />
+    );
+  }
+
+  if(!pins?.length) return <h2>No pins available</h2>
+
+  return (
+    <div>
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
+    </div>
+  )
+}
+
+export default Feed
